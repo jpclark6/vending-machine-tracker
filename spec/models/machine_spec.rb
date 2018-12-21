38dff354760f076291890808e7490f723a278fb3
jpclark6 <jpclark6@gmail.com>
2018-12-21 10:35:57 -0700

    passes vending machine test

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -7,4 +7,17 @@ describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through(:machine_snacks) }
   end
+  describe 'instance methods' do
+    before(:each) do
+      @sam = Owner.create(name: "Sam's Snacks")
+      @machine_1 = @sam.machines.create(location: "Don's Mixed Drinks")
+      @machine_2 = @sam.machines.create(location: "Turing Basement")
+      @snack_1 = Snack.create(name: 'Snickers', price: 2, machines: [@machine_1, @machine_2])
+      @snack_2 = Snack.create(name: 'Mars', price: 10, machines: [@machine_1])
+      @snack_3 = Snack.create(name: 'Soda', price: 1, machines: [@machine_2])
+    end
+    it 'can calculate average price in machine' do
+      expect(@machine_2.average_price).to eq(1.5)
+    end
+  end
 end
